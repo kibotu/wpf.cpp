53979d75db8c1ee30873b8682deea053911cfa2a
s39036 <janrabe@beuth-hochschule.de>
2013-04-15 18:18:54 +0200

    no pro.user files

--- uebung1/uebing11/fractions/fractions.pro
+++ uebung1/uebing11/fractions/fractions.pro
@@ -3,5 +3,10 @@ CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += main.cpp
+SOURCES += \
+    rationalnumber.cpp \
+    testRN.cpp
+
+HEADERS += \
+    rationalnumber.h
 
--- uebung1/uebing11/fractions/rationalnumber.h
+++ uebung1/uebing11/fractions/rationalnumber.h
@@ -2,3 +2,32 @@
 #define RATIONALNUMBER_H
 
 #endif // RATIONALNUMBER_H
+
+/**
+ * @brief Represents a rational number.
+ *
+ * Numerator
+ * ---------
+ * Denominator
+ */
+struct RationalNumber {
+
+    /**
+     * @brief Zähler
+     */
+    int numerator;
+
+    /**
+     * @brief Nenner
+     */
+    int denominator;
+};
+
+/**
+ * @brief Validates a RationalNumber against division by zero.
+ * (Ignores integer overflow)
+ *
+ * @param n - RationalNumber to be validated.
+ * @return true if is valid.
+ */
+bool rnIsValid(RationalNumber n);
